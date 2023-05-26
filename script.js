2895d54d1dbe911852c5156d3a7a3b7158e69902
GreetyCr <randallgbc@gmail.com>
2023-05-26 10:45:19 -0600

    Add email.js
    Add email.js
    Add script index.html (For tests)
    Update class index of modal

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 // template_bhy8kmj
 // service_ewm39wj
-// 
+// nuA-SiNznzXxQhc40
 
 
 function contact(event) {
   event.preventDefault();
-  emailjs
-  .sendForm(
-    'service_ewm39wj',
-    'template_bhy8kmj',
-    event.target,
-    'nuA-SiNznzXxQhc40'
-  ).then(() => {
-    'this worked'
-  })
+  // emailjs
+  // .sendForm(
+  //   'service_ewm39wj',
+  //   'template_bhy8kmj',
+  //   event.target,
+  //   'nuA-SiNznzXxQhc40'
+  // ).then(() => {
+  //   console.log('this worked')
+  // })
 
+  const loading = document.querySelector('.modal__overlay--loading');
+  const success = document.querySelector('.modal__overlay--success');
+  loading.classList += " modal__overlay--visible"
+  setTimeout(() => { 
+    loading.classList.remove("modal__overlay--visible")
+    console.log("It worked")
+  }, 1000)
 }
+
