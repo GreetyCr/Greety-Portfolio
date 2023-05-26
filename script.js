c183c387eca4cba777d87e46ace5a37ae925dd28
GreetyCr <randallgbc@gmail.com>
2023-05-26 13:13:24 -0600

    Update script modal

--- script.js
+++ script.js
@@ -2,26 +2,24 @@
 // service_ewm39wj
 // nuA-SiNznzXxQhc40
 
-
 function contact(event) {
   event.preventDefault();
-  // emailjs
-  // .sendForm(
-  //   'service_ewm39wj',
-  //   'template_bhy8kmj',
-  //   event.target,
-  //   'nuA-SiNznzXxQhc40'
-  // ).then(() => {
-  //   console.log('this worked')
-  // })
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
 
-  const loading = document.querySelector('.modal__overlay--loading');
-  const success = document.querySelector('.modal__overlay--success');
-  loading.classList += " modal__overlay--visible"
-  setTimeout(() => { 
-    loading.classList.remove("modal__overlay--visible")
-    success.classList += " modal__overlay--visible"
-    console.log("It worked")
-  }, 1000)
+  emailjs
+    .sendForm(
+      "service_ewm39wj",
+      "template_bhy8kmj",
+      event.target,
+      "nuA-SiNznzXxQhc40"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    }).catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert("The email service is temporary unavailable. Please contact me directly on greetysoftwarejs@gmail.com")
+    })
 }
-
