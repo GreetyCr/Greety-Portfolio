0922e5341a2e4ac551b99e699b8b4dd997853e3f
GreetyCr <randallgbc@gmail.com>
2023-06-14 14:20:40 -0600

    Add shapes movement

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 let isModalOpen = false;
 let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for(let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const oddInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = 'translate('+(x * oddInt)+'px,'+(y * oddInt)+'px)';
+  }
+}
 
 function toggleContrast() {
   contrastToggle = !contrastToggle;
